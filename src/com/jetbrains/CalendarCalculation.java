0cc0fbe1b9f639d01528c2cc5a528d13a6dd69f9
Jeffrey Blumenkopf <jeffrey.blumenkopf@gmail.com>
2017-01-23 15:57:36 -0500

    Adds ReverseNumberCount Program

--- src/com/jetbrains/CalendarCalculation.java
+++ src/com/jetbrains/CalendarCalculation.java
@@ -1,7 +1,27 @@
 package com.jetbrains;
+import java.util.Calendar;
+
+import static java.util.Calendar.*;
+
 
 /**
  * Created by Owner on 1/23/2017.
  */
-public class CalendarCalculation {
+class CalendarCalculation {
+    public static void main (String args[]){
+        System.out.println("code runs!");
+    /*    Calendar rightNow =  new Calendar();
+    System.out.print(rightNow);
+
+    set(rightNow.MONTH, rightNow.SEPTEMBER);
+    */
+
+
+
+
+
+
+
+
+    }
 }
--- src/com/jetbrains/Project1.java
+++ src/com/jetbrains/Project1.java
@@ -1,7 +1,74 @@
 package com.jetbrains;
 
+import java.util.Scanner;
+
 /**
  * Created by Owner on 1/22/2017.
  */
-public class Project1 {
-}
+//public class Project1 {package com.company;
+//import java.util.Scanner;
+
+    /**
+     * Created by Owner on 1/19/2017.
+     */
+    class ReverseCountJavaBootcamp {
+        public static void main (String[] args){
+
+
+            int userNum =  0; // stores entire number that will be reversed
+            int i          =  0; // iteration count outer loop
+            int x          =  0; // iteration count inner loop
+            String userStringNum  = ""; // holds the entire number as a string
+            Character firstUserNum = '_';  // holds first digit of userNumber
+            Character secondUserNum = '-'; // holds second digit of userNumber
+            Character thirdUserNum  = '-'; // holds third digit of userNumber
+            Character fourthUserNum = '-'; // holds fourth digit of userNumber
+
+
+            System.out.println("Type a number: ");
+            Scanner scnr = new Scanner(System.in);
+            userNum = scnr.nextInt();
+            userStringNum = userStringNum.valueOf(userNum);
+
+
+            //  for loop runs for the amount of times of the length of string minus 1
+            //  if statement holds the value of each Character of the string in a different character
+
+            for (i = 0; i < userStringNum.length(); ++i){
+
+                if( (i == 0) && (userStringNum.length() >= 1 ) ){
+
+                    firstUserNum =  userStringNum.charAt(i);
+                }
+                if( (i == 1) && (userStringNum.length() >= 2 ) ){
+
+                    secondUserNum = userStringNum.charAt(i);
+                }
+                if( (i == 2) && (userStringNum.charAt(i) >= 3 ) ){
+
+                    thirdUserNum = userStringNum.charAt(i);
+
+                }
+                if( (i == 3) && (userStringNum.charAt(i) >= 4 ) ){
+
+                    fourthUserNum = userStringNum.charAt(i);
+
+                }
+
+            }
+
+
+            System.out.print("Your number reversed is: ");
+            System.out.print(fourthUserNum);
+            System.out.print(thirdUserNum);
+            System.out.print(secondUserNum);
+            System.out.print(firstUserNum);
+
+
+
+
+
+        }
+    }
+
+
