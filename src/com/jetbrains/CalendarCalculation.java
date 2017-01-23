80b6e9c29299150e100c32cf4a83636d500f2fb6
Jeffrey Blumenkopf <jeffrey.blumenkopf@gmail.com>
2017-01-23 15:06:10 -0500

    First Commit

---
+++ src/com/jetbrains/CalendarCalculation.java
@@ -0,0 +1,7 @@
+package com.jetbrains;
+
+/**
+ * Created by Owner on 1/23/2017.
+ */
+public class CalendarCalculation {
+}
---
+++ src/com/jetbrains/Project1.java
@@ -0,0 +1,7 @@
+package com.jetbrains;
+
+/**
+ * Created by Owner on 1/22/2017.
+ */
+public class Project1 {
+}
